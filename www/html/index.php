15c09b039a7c41672a45d923f3834a3adb471aef
soymusta <mustaelhachmimahti@gmail.com>
2022-11-18 14:04:58 +0100

    PHP woks correctly

---
+++ www/html/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>  
+     <head>  
+      <title>This is  wordpress from MUSTA!</title>
+     </head>  
+
+     <body>  
+      <h1>This is  wordpress from MUSTA!</h1>
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+     </body>
